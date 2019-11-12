3251dffb767030182b72b7a7316fa632eae60997
Issac Moreno <issaxavier909@gmail.com>
2019-11-11 17:00:12 -0800

    Intitial Commit

--- index.js
+++ index.js
@@ -39,7 +39,13 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
+function makePersonObject(id, name, email) {
+  const person = {
+    id: id, 
+    name: name, 
+    email: email
+  };
+  return person;
   /* code here */
 }
 
